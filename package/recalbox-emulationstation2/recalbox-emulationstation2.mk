8a81a51c88d65227e2fc1356061933979059052d
MagicSeb <jeremiah252@hotmail.com>
2020-01-16 21:33:15 +0100

    Moved patches to build things, updated emulationstation to latest commit IMPORTANT : You will be unable to use recalbox as usual, a bug about saving settings is still here !

--- package/recalbox-emulationstation2/recalbox-emulationstation2.mk
+++ package/recalbox-emulationstation2/recalbox-emulationstation2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RECALBOX_EMULATIONSTATION2_VERSION = 8d063a1845c63c3dd9ecdea11367be8a8f0941ad
+RECALBOX_EMULATIONSTATION2_VERSION = e7cbf94287ceb7b5140ba8516b0444f28f04b523
 RECALBOX_EMULATIONSTATION2_SITE = https://gitlab.com/recalbox/recalbox-emulationstation.git
 RECALBOX_EMULATIONSTATION2_SITE_METHOD = git
 RECALBOX_EMULATIONSTATION2_LICENSE = MIT
